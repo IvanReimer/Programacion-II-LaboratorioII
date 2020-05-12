3e5c4fd97e668680b039b801042e8fe8a3dc82c9
ivanreimer <ivanreimer1@gmail.com>
2020-05-12 17:54:47 -0300

    Trabajo Practico 2 corregido

---
+++ RecupetaroriosTP/TP2/Entidades/Automovil.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        #region Enumerados
+        public enum ETipo { Monovolumen, Sedan }
+        #endregion
+
+        #region Atributos
+        private ETipo tipo;
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca">Marca del automovil</param>
+        /// <param name="chasis">Chasis del automovil</param>
+        /// <param name="color">Color del automovil</param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color) : this(marca, chasis, color,ETipo.Monovolumen)
+        {
+        }
+        /// <summary>
+        /// Se le agrega constructor con parámetro tipo para poder settearlo mejor.
+        /// </summary>
+        /// <param name="marca">Marca del automovil</param>
+        /// <param name="chasis">Chasis del automovil</param>
+        /// <param name="color">Color del automovil</param>
+        /// <param name="tipo">Tipo del automovil</param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) : base(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Agrega datos del automovil al método Mostrar de vehiculo.
+        /// </summary>
+        /// <returns>Los datos del vehiculo</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine(String.Format("TAMAÑO : {0}", this.Tamanio));
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+            return sb.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ RecupetaroriosTP/TP2/Entidades/Camioneta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Camioneta : Vehiculo
+    {
+
+        #region Constructores
+        /// <summary>
+        /// Constructor necesario para inicializar atributos base.
+        /// </summary>
+        /// <param name="marca">Marca del camion</param>
+        /// <param name="codigo">Codigo del camion</param>
+        /// <param name="color">Color del camion</param>
+        public Camioneta(EMarca marca, string codigo, ConsoleColor color) : base(marca, codigo, color)
+        {
+        }
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Agrega, además de los datos de Vehículo, los datos adicionales de la camioneta.
+        /// </summary>
+        /// <returns>Los datos de la camioneta en forma de string</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CAMIONETA");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine(String.Format("TAMAÑO : {0}", this.Tamanio));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
